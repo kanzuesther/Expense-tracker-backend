01ec93a24e56cc95370a7dbe6b732356b5015e8b
kanzu esther <kanzues65@gmail.com>
2024-08-17 10:01:47 +0000

    new version of backend

--- controllers/income.js
+++ controllers/income.js
@@ -1,53 +1,60 @@
-const IncomeSchema= require("../models/IncomeModel")
+const IncomeSchema = require("../models/IncomeModel")
 
-exports.addIncome= async(req,res) =>{
+exports.addIncome = async (req, res) => {
     console.log(req.body)
-    const {title, amount, category, description,date} = req.body
+    const { title, amount, category, description, date } = req.body
 
-    const income= IncomeSchema({
+    const income = IncomeSchema({
         title,
         amount,
         category,
         description,
         date
     })
-    try{
+    try {
         //validations
-        if(!title|| !category|| !description||!date){
-        return res.status(400).json({message:'All fields are required!'})
-    }
-    if(amount<=0 || !amount ==='number'){
-        return res.status(400).json({message:'Amount must be a postive number!'})
-    }
-    await income.save()
-    res.status(200).json({message: 'Income Added', data:income})
+        if (!title || !category || !description || !date) {
+            return res.status(400).json({ message: 'All fields are required!' })
+        }
+        if (amount <= 0 || !amount === 'number') {
+            return res.status(400).json({ message: 'Amount must be a postive number!' })
+        }
+        await income.save()
+        const incomes = await IncomeSchema.find().sort({ createdAt: -1 })
+        res.status(200).json({ message: 'Income Added', data: incomes });
 
-    } catch(error){
-        res.status(500).json({message:error.toString()})
+    } catch (error) {
+        res.status(500).json({ message: error.toString() })
 
     }
     console.log(income)
 }
 
-exports.getIncome= async(req,res) =>{
-    try{
-        const incomes= await IncomeSchema.find().sort({createdAt: -1})
+exports.getIncome = async (req, res) => {
+    try {
+        const incomes = await IncomeSchema.find().sort({ createdAt: -1 })
         res.status(200).json(incomes)
 
-    } catch(error){
-        res.status(500).json({message:'Server error'})
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' })
 
     }
 }
 
 
-exports.deleteIncome= async(req,res) =>{
-    const{id} = req.params;
-    IncomeSchema.findByIdAndDelete(id)
-    .then((income)=>{
-        res.status(200).json({message:'Successfully deleted', data:income})
+exports.deleteIncome = async (req, res) => {
+    const { id } = req.params;
+    IncomeSchema.findByIdAndDelete(id).then( async (income)=>{
+        const incomes = await IncomeSchema.find().sort({ createdAt: -1 })
+        res.status(200).json({ message: 'Successfully deleted', data: incomes });    
     })
-    .catch((error)=>{
-        res.status(500).json({message:'Server Error',error:error.toString()})
-    })
+    .catch((error) => {
+        res.status(500).json({ message: 'Server Error', error: error.toString() })
+    });
+
+   
+        // .then((income) => {
+        //     res.status(200).json({ message: 'Successfully deleted', data: income })
+        // })
+
 }
